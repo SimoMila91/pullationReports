708987796ac26c6297a1ddaef1c246e43adcc650
Simone <simomilanesio5@gmail.com>
2021-07-26 22:42:40 +0200

    finished report with with upload image and create a get to retrieve the pollutions from the server

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Button, Container, Header } from 'semantic-ui-react';
+import React, { useState, useMemo, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Form, Button, Container, Header, Dropdown, Icon, Message } from 'semantic-ui-react';
+import countryList from 'react-select-country-list';
+import Compress from 'react-image-file-resizer';
+import axios from 'axios';
+
+
 
 const formStyle = {
   width: '36rem',
   margin: 'auto',
+  textAlign: 'left'
 };
 
 const button = {
@@ -11,62 +18,158 @@ const button = {
 };
 
 export default function Report() {
-
-  const [data, setData] = useState({
-    city: '',
-    stateReport: '', 
-    address: ''
+  const [city, setCity] = useState('');
+  const [address, setAddress] = useState('');
+  const [country, setCountry] = useState('');
+  const [control, setControl] = useState(false);
+  const [name, setName] = useState();
+  const [file, setFile] = useState();
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState({
+    variant: false,
+    message: ''
   });
-  
+  let history = useHistory();
+
+  const options = useMemo(() => countryList().getData(), []);
+
   const onSubmit = e => {
     e.preventDefault();
-    console.log('inviato');
+    const data = new FormData();
+    data.append("name", name);
+    data.append("file", file);
+    data.append("city", city);
+    data.append("address", address);
+    data.append("country", country);
+
+    axios.post("http://localhost:3000/upload", data)
+      .then(res => {
+        setMessage({
+          variant: true,
+          message: res.data
+        });
+        setTimeout(() => history.push('/'), 4000);
+      })
+      .catch(err => console.log(err));
   }; 
 
-  const handleChange = e => {
-    setData({...data, [e.target.name]: e.target.value });
+  useEffect(() => {
+    if  (city !== "" &&  address !== "" && country !== "" && name !== undefined) {
+      setControl(true); 
+    } else {
+      setControl(false);
+    }
+  }, [city, address, country, name]);
+
+  const changeHandler = (e, value) => {
+    let res = countryList().getLabel(value.value);
+    setCountry(res);
+  };
+
+  const fileChange = e => {
+    const fileData = e.target.files[0]; 
+    const ext = fileData.name.split('.').pop();
+    if (ext.match(/(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
+      Compress.imageFileResizer(
+        fileData, 
+        480,
+        480,
+        "JPEG, PNG, JPG",
+        70, 
+        0, 
+        (uri) => {
+          console.log(uri);
+          setName(fileData.name);
+          setFile(fileData);
+          setError(false);
+        },
+        "base64"
+      );
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <Container>
-      <Header as='h2'>Where do you want to report the pollution?</Header>
-      <div style={formStyle} className="marginForm">
-        <Form onSubmit={onSubmit}>
-          <Form.Group widths="equal">
-            <Form.Input 
-              required
-              name="stateReport"
-              size="mini"
-              label="State"
-              type="text"
-              value={data.stateReport}
-              onChange={handleChange}
-            />
-            <Form.Input 
-              required
-              name="city"
-              size="mini"
-              label="City"
-              type="text"
-              value={data.city}
-              onChange={handleChange}
-            />
-            <Form.Input 
-              required
-              name="address"
-              size="mini"
-              label="Address"
-              type="text"
-              value={data.address}
-              onChange={handleChange}
-            /> 
-          </Form.Group>
-          <Form.Group>
-            <Button type="submit" className="marginForm" style={button}>Send Report</Button>
-          </Form.Group>
-        </Form>
+      {
+        !message.variant ? 
+        <>
+          <Header as='h2'>Where do you want to report the pollution?</Header>
+          <div style={formStyle} className="marginForm">
+            <Form onSubmit={onSubmit}>
+              <Form.Field>
+                <label>Country</label>
+                <Dropdown
+                  placeholder="Select country"
+                  fluid
+                  options={options}
+                  text={country}
+                  selection
+                  onChange={changeHandler}
+                />
+              </Form.Field>  
+              <Form.Group widths="equal" className="marginForm">
+              <Form.Input 
+                  size="mini"
+                  label="City"
+                  value={city}
+                  onChange={e => setCity(e.target.value)}
+                /> 
+                <Form.Input 
+                  size="mini"
+                  label="Address"
+                  value={address}
+                  onChange={e => setAddress(e.target.value)}
+                /> 
+              </Form.Group>
+              <Form.Field className="marginForm">
+              <label>Pollution's image</label>
+                    <Button as="label" htmlFor="file" type="button" animated="fade">
+                      <Button.Content visible>
+                        <Icon name="file" />
+                      </Button.Content>
+                      <Button.Content hidden>Choose a File</Button.Content>
+                    </Button>
+                    <input
+                      type="file"
+                      id="file"
+                      hidden
+                      onChange={fileChange}
+                    />
+                    <Form.Input
+                      className="marginForm"
+                      fluid
+                      label="File Chosen: "
+                      placeholder="Use the above bar to browse your file system"
+                      readOnly
+                      value={name}
+                      error={ error ? { 
+                        content: 'You may just upload images',
+                        pointing: 'below',
+                      } : null }
+                    />
+              </Form.Field>
+              <Form.Group>
+                {
+                  !control ?  
+                    <Button style={button} className="marginForm"  disabled>Send Report</Button>
+                  : 
+                    <Button type="submit" style={button} className="marginForm" primary>Send Report</Button> 
+                }
+              </Form.Group>
+            </Form>
+          </div>
+      </>
+      : 
+      <div className="messagePos">
+        <Message 
+            success
+            header={message.message}
+            content="you will be redirected to the home page in 4 seconds"
+       />
       </div>
-      
+      }
     </Container>
   );
 }
--- src/components/ReportList.jsx
+++ src/components/ReportList.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container } from 'semantic-ui-react';
+import axios from 'axios';
 
 export default function ReportList() {
-  return <h1>Report</h1>;
+  const [reports, setReports] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/reports")
+      .then(res => {
+        console.log(res);
+        setReports(res.data);
+      }).catch(err => console.log(err));
+  }, [])
+  
+  return (
+    <Container>
+      {
+        reports.map((report, i) => ( 
+          <img key={i} width="500" height="500" src={`http://localhost:3000/images/${report.image}`} alt="ciao" />
+        ))
+      }
+    </Container>
+  )
 }
